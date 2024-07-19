76ee6e20cc2098a6ada511582f3c5cf879650063
Alsayed Nimer <nimer.alsayed@sos-palestine.org>
2024-07-19 14:53:07 +0300

    NavBar

---
+++ src/main.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+)
+document.getElementsByTagName('html')[0].setAttribute("dir", "rtl");
+//Or you can do it where ever you want, base on user selected language, or something else 
+//serviceWorker.unregister();
